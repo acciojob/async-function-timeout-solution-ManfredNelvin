86007127a157d6080b3ac3e796fd4080baa4a85e
ManfredNelvin <119521670+ManfredNelvin@users.noreply.github.com>
2023-06-24 15:51:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ const delay = document.getElementById("delay");
 const btn = document.getElementById("btn");
 const output = document.getElementById("output");
 
-//your code here
 async function showMessage() {
   const message = text.value;
-  const delayVal = delay.value;
+  const delayVal = delay.value * 1000; // Convert delay to milliseconds
   await new Promise((resolve) => setTimeout(resolve, delayVal));
   output.innerText = message;
 }
